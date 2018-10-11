5a30d0bb64d6507cb29882a91b2447d438eefac5
Carlos Estrada <carlos.deg02@hotmail.com>
2018-10-10 20:28:59 -0500

    Added basic classes

--- RobotCode/robot.ino
+++ RobotCode/robot.ino
@@ -1,10 +1,26 @@
 
-
 class Motor
 {
 public:
-  Motor();
+  Motor(byte, byte, byte byte);
 
 private:
   byte pinA, pinB;
+  byte encA, encB;
 };
+
+Motor::Motor(byte pinA, byte pinB, byte encA, byte encB)
+{
+  this->pinA = pinA;
+  this->pinB = pinB;
+  this->encA = encA;
+  this->encB = encB;
+}
+
+class Camera
+{
+public:
+  Camera();
+
+private:
+}
